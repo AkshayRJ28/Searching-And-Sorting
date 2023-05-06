2acd8e45487e41fa6130f169a706010734ff73e0
AkshayRJ28 <118600264+AkshayRJ28@users.noreply.github.com>
2023-05-06 15:29:01 +0530

    Update PeakIndexInMountainArray.java

--- PeakIndexInMountainArray.java
+++ PeakIndexInMountainArray.java
@@ -19,38 +19,6 @@ public class PeakIndexInMountainArray {
 			else
 				e=mid;
 		}
-		//System.out.println("Peak element in array is : "+a[s]);
-		
-		pivotIndex();
-
+		System.out.println("Peak element in array is : "+s);
 	}
-	
-	public static void pivotIndex()
-	{
-		int a[]= {1,7,3,6,5,1};
-		
-		int total =0;
-		int leftSum=0;
-		int index =0;
-		
-		for(int i=0; i<a.length;i++)
-		{
-			total +=a[i];
-		}
-		
-		for(int i=0; i<a.length;i++)
-		{
-			int rightSum = total - a[i]-leftSum;
-			
-			if(rightSum==leftSum)
-			{
-				index =i;
-			}
-			
-			leftSum += a[i];
-		}
-		
-		System.out.println(leftSum);
-	}
-
 }
